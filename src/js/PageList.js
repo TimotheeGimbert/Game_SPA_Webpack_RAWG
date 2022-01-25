ea3de54658db488b4db57b7ac8e611878f5fe844
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-25 18:01:50 +0100

    adds renders pages and API connect

--- src/js/PageList.js
+++ src/js/PageList.js
@@ -1,5 +1,45 @@
 const PageList = (argument = '') => {
   console.log('PageList', argument);
-}
+
+  const preparePage = () => {
+    const cleanedArgument = argument.replace(/\s+/g, "-");
+
+    const displayResults = (results) => {
+      const resultsContent = results.map((article) => (
+        `<article class="cardGame">
+          <h1>${article.name}</h1>
+          <h2>${article.released}</h2>
+          <a href="#pagedetail/${article.id}">${article.id}</a>
+        </article>`
+      ));
+      const resultsContainer = document.querySelector(".page-list .articles");
+      resultsContainer.innerHTML = resultsContent.join("\n");
+    };
+
+    const fetchList = (url, argument) => {
+      const finalURL = argument ? `${url}&search=${argument}` : url;
+      fetch(finalURL)
+        .then((response) => response.json())
+        .then((responseData) => {
+          displayResults(responseData.results)
+        });
+    };
+
+    fetchList(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`, cleanedArgument);
+  };
+
+  const render = () => {
+    pageContent.innerHTML = `
+      <section class="page-list">
+        <div class="articles">...loading</div>
+      </section>
+    `;
+
+    preparePage();
+  };
+
+  render();
+};
+
 
 export { PageList };
