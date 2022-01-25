f7d94b1cbdb901fe6efe66ee65438679775bf010
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-25 14:56:39 +0100

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import '../style/index.scss';
+import 'bootstrap';
+
+const testBabel = () => {
+  console.log('Babel should transform this arrow function for ES5 in /dist bundled js file');
+};
+console.log('Hello');
+testBabel();
+
+import { msgObject, logMessage } from './file1';
+logMessage(msgObject.message);
+
+import tool from './file2';
+tool.logMessage(tool.msgObject.message);
+
+import dayjs from 'dayjs';
+console.log(dayjs().format('MMMM DD YYYY')); 
+console.log(dayjs().subtract(10, 'days').format('DD/MM/YYYY'));
+
+console.log(" >>>>>>>>>>>>>>>>< DOTENV .env secret variable (DONT PUSH IT, PLACE INTO .gitignore) : ", process.env.mySecretVar);
+
+import { buttonRemover } from './domManip';
+buttonRemover();
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env) => {
+
+  console.log(" >>>>>>>>>>>>>>> NODE_ENV from webpack.config.js:", env.NODE_ENV);
+
+  return {
+    entry: './src/js/index.js',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                implementation: require('sass'),
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'bundle.css',
+      }),
+      new Dotenv(),
+    ],
+
+    mode: 'development',
+
+    watch: true,
+  
+  } ;
+};
